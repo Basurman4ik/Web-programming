ae8af773e0da0b3d5466e8a08747863b368565c2
Basurman4ik <47212075+Basurman4ik@users.noreply.github.com>
2022-11-07 19:16:19 +0200

    Add files via upload

---
+++ Website_Savchenko/script.js
@@ -0,0 +1,63 @@
+let result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("Anton Huleha.");
+} else {
+	end();
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("Personal page of professional 3D artist and texture artist.");
+} else {
+	alert("Anton Huleha.");
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("About me. In 2007 graduated from University of customs and finance in Dnipro for Computer science and 3D technologies. Worked with worldwide known gamedeveloping companies for more then 10 years. Have a lot of expirience in creating gameready assets and is ready to share that knowledge with everybody who really want.");
+} else {
+	alert("Personal page of professional 3D artist and texture artist.");
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("My previous projects During career i worked with such well-known companies as DigitalMindsoft, Barbed Wire Studious and Ubisoft as Lead Seniour 3D artist. Took part in creating such games as Call to Arms, Gates of Hell and Men of War: Assault squad.");
+} else {
+	alert("About me. In 2007 graduated from University of customs and finance in Dnipro for Computer science and 3D technologies. Worked with worldwide known gamedeveloping companies for more then 10 years. Have a lot of expirience in creating gameready assets and is ready to share that knowledge with everybody who really want.");
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("My professional skills Have excelent knodledge in texturing and 3D modelling and different software such as Blender, Substance Painter, RizomUV UnrealCreator and I am ready to teach you how to become a professional.");
+} else {
+	alert("My previous projects During career i worked with such well-known companies as DigitalMindsoft, Barbed Wire Studious and Ubisoft as Lead Seniour 3D artist. Took part in creating such games as Call to Arms, Gates of Hell and Men of War: Assault squad.");
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("Contact with me Phone: +1234567890 Gmail: contact@me Location: Dnipro, Central");
+} else {
+	alert("My professional skills Have excelent knodledge in texturing and 3D modelling and different software such as Blender, Substance Painter, RizomUV UnrealCreator and I am ready to teach you how to become a professional.");
+}
+
+result = confirm("Continue ? ");
+
+if (result == true) {
+	alert("© 2022 All right reserved | Contact: Mail me");
+} else {
+	alert("Contact with me Phone: +1234567890 Gmail: contact@me Location: Dnipro, Central");
+}
+
+result = confirm("Exit? ");
+
+if (result == true) {
+	end();
+} else {
+	alert("© 2022 All right reserved | Contact: Mail me");
+}
